ca3ec883d9d18cc222f8c0ac4d615651a29559eb
snehal03 <snehaldhane03@gmail.com>
2022-03-23 20:13:36 +0530

    product-curd-usingfiles

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const adminRoutes= require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 const app = express();
 
-
-app.set('view engine', 'ejs'); // allow use to set any value globally on express app
+app.set("view engine", "ejs"); // allow use to set any value globally on express app
 
 /**
  * set view location by default root/views but if we have diff name then mandatory to set
  */
-app.set('views', 'views')
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Use routes
  */
-app.use('/admin',adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,34 +1,78 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
-exports.getAddProducts = (req,res)=>{
-    // res.sendFile(path.join(rootDir, 'views','add-product.html'));
-    res.render('admin/add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-    });
+exports.getAddProducts = (req, res) => {
+  // res.sendFile(path.join(rootDir, 'views','add-product.html'));
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+    editing: false,
+  });
 };
 
-exports.postAddProducts = (req,res)=>{
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
-    const description = req.body.description;
-    const product = new Product(title,imageUrl,description,price);
-    product.save();
-    // products.push({title: req.body.title})
-    res.redirect('/');
-}
+exports.postAddProducts = (req, res) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save();
+  // products.push({title: req.body.title})
+  res.redirect("products");
+};
 
-
-exports.getProducts = (req,res)=>{
-    Product.fetchAll((products)=>{
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products'
-          });
+exports.getEditProduct = (req, res) => {
+  const editMode = req.query.edit;
+  const productId = req.params.productId;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  Product.findById(productId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
     });
+  });
+};
+
+exports.postEditProducts = (req, res) => {
+  const productId = req.body.productId;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const updatedProduct = new Product(
+    productId,
+    title,
+    imageUrl,
+    description,
+    price
+  );
+  updatedProduct.save();
+  res.redirect("products");
+};
+
+exports.getProducts = (req, res) => {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};
+
+exports.postDeleteProducts = (req, res) => {
+  const productId = req.body.productId;
+  Product.findById(productId, (product) => {
+    Product.deletById(productId, product.price);
+    res.redirect("products");
+  });
 };
