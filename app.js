ec8a3da67774a414c1d20c4f3e041f46d9986807
snehal03 <snehal.dhane11@gmail.com>
2022-03-24 12:12:59 +0530

    session-login-logout (#2)
    Co-authored-by: snehal03 <snehaldhane03@gmail.com>

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/error");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
-app.set("view engine", "ejs"); // allow use to set any value globally on express app
+app.set('view engine', 'ejs'); // allow use to set any value globally on express app
 
 /**
  * set view location by default root/views but if we have diff name then mandatory to set
  */
-app.set("views", "views");
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret' , resave:false, saveUninitialized: false}))
 
 /**
  * Use routes
  */
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
