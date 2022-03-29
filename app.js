c9b4440169fa002ff12c9a0d7fe51befa8bb20b4
snehal03 <snehaldhane03@gmail.com>
2022-03-29 12:14:22 +0530

    added logger and env

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const path = require('path');
 const session = require('express-session');
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const helmet = require('helmet'); // add secure headers
+const compression = require('compression'); // compress data
+const morgan = require('morgan'); // add logs
+const fs = require('fs');
+
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const authRoutes = require('./routes/auth');
 
+
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname,'access.log'),
+    {flags: 'a'}
+)
+
 const app = express();
+app.use(morgan('combined', {stream: accessLogStream}));
 const csrfProtection = csrf();
 
 app.set('view engine', 'ejs'); // allow use to set any value globally on express app
@@ -20,9 +32,10 @@ app.set('view engine', 'ejs'); // allow use to set any value globally on express
  */
 app.set('views', 'views');
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret' , resave:false, saveUninitialized: false}))
+app.use(express.static(path.join(__dirname, process.env.PUBLIC_DIR)));
+app.use(session({secret: process.env.SESSION_SECRETE_KEY , resave:false, saveUninitialized: false}))
 /**
  * checks every post request for csrf token
  */
@@ -39,13 +52,17 @@ app.use((req,res,next)=> {
     next();
 });
 
+
 /**
  * Use routes
  */
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-
 app.use(errorController.get404);
 
-app.listen(3000);
+app.use(helmet()); // used to set secure headers
+app.use(compression()); // compress the resources
+
+
+app.listen(process.env.PORT || 3000);
